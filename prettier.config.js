983efa4beb7957969abfcb6ebfbe0f9dfd622bc2
Jeff Eaton <jeff@eaton.fyi>
2024-09-12 19:24:29 -0500

    ESLint flat config and TSX

--- prettier.config.js
+++ prettier.config.js
@@ -3,11 +3,9 @@
 const config = {
   semi: true,
   singleQuote: true,
-  arrowParens: "avoid",
-  trailingComma: "all",
-  plugins: [
-    "prettier-plugin-organize-imports"
-  ]
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-organize-imports']
 }
 
 export default config;
